aa8a10e77ab230586ec5faf1985fb76e8ef158b2
王晓丽 <cocoder12138@gmail.com>
2022-06-11 22:11:17 +0800

    抓取所有 pdf 的链接，点击download 后添加下载任务

--- background.js
+++ background.js
@@ -1,34 +1,10 @@
-console.log("extension");
+chrome.runtime.onMessage.addListener(function (event) {
+  if (event.type === "down_all") {
+    console.log(event);
+    const { links } = event;
 
-chrome.runtime.onMessage.addListener(function (arg, sender, sendResponse) {
-  var args = arg.collection;
-
-  console.log("message message");
-  console.log(args);
-
-  //   chrome.downloads.download({
-  //     url: img_url,
-  //     filename: saveas,
-  //     saveAs: false,
-  //   });
+    links.forEach((link) => {
+      chrome.downloads.download({ url: link.url, filename: link.name });
+    });
+  }
 });
-// const getLinks = () => {
-//   links = Array.from(document.querySelectorAll("a"))
-//     .filter((v) => v.href.endsWith(".pdf"))
-//     .map((v) => v.href);
-
-//   return links;
-// };
-
-// document.onreadystatechange = () => {
-//   if (document.readyState === "complete") {
-//     const links = getLinks();
-//     console.log(links);
-//     // chrome.downloads.open(links[0]);
-//     //   chrome.downloads.download(
-//     //     "https://courses.d2l.ai/zh-v2/assets/pdfs/part-0_1.pdf"
-//     //   );
-//   }
-// };
-
-// console.log(getLinks());
--- popup.js
+++ popup.js
@@ -1,24 +1,18 @@
-function downloadPDF() {
-  var downloadUrl = "https://courses.d2l.ai/zh-v2/assets/pdfs/part-0_1.pdf";
+function getPDFLinks() {
+  const links = Array.from(document.querySelectorAll("a"))
+    .filter((v) => v.href.endsWith(".pdf"))
+    .map((v) => ({ name: v.text || v.href.split("/")[-1], url: v.href }));
 
-  console.log("download pdf start!");
-
-  const param = {
-    url: downloadUrl,
-    filename: "1.pdf",
-    conflictAction: "uniquify",
-  };
-  chrome.runtime.sendMessage(param);
+  chrome.runtime.sendMessage({ type: "down_all", links });
 }
 
 let btn = document.getElementById("download");
 
-// When the button is clicked, inject setPageBackgroundColor into current page
 btn.addEventListener("click", async () => {
   let [tab] = await chrome.tabs.query({ active: true, currentWindow: true });
 
   chrome.scripting.executeScript({
     target: { tabId: tab.id },
-    function: downloadPDF,
+    function: getPDFLinks,
   });
 });
